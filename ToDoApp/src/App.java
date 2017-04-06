2007d2e751e3dd2e7d6490daa96fbb68065bab36
Kristof Majoros <akmajoros1@gmail.com>
2017-04-06 14:25:42 +0200

    assigned methods to ToDoList class and App calls the methods from there

--- ToDoApp/src/App.java
+++ ToDoApp/src/App.java
@@ -8,33 +8,14 @@ public class App {
 
     public static void main(String[] args) {
 
+      ToDoList toDoList = new ToDoList();
+
       if (args.length == 0) {
-        Path filePath = Paths.get("PrintUsage.txt");
-        try {
-          List<String> lines = Files.readAllLines(filePath);
-          for (int i = 0; i < lines.size(); i++) {
-            System.out.println(lines.get(i));
-          }
-        } catch (Exception e) {
-          e.printStackTrace();
-          System.out.println("Uh-oh, could not read the file!");
-        }
+        toDoList.printUsage();
       }
 
       if (args.length > 0 && Arrays.toString(args).contains("-l")) {
-        Path filePath = Paths.get("list.txt");
-        try {
-          List<String> lines = Files.readAllLines(filePath);
-          if (lines.size() == 0) {
-            System.out.println("No todos for today");
-          }
-          for (int i = 0; i < lines.size(); i++) {
-            System.out.println(i+1 + " - " + lines.get(i));
-          }
-        } catch (Exception e) {
-          e.printStackTrace();
-          System.out.println("Uh-oh, could not read the file!");
-        }
+        toDoList.printToDo();
       }
     }
 }
--- ToDoApp/src/ToDoList.java
+++ ToDoApp/src/ToDoList.java
@@ -1,3 +1,34 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class ToDoList {
 
+  public void printUsage() {
+    Path usagePath = Paths.get("PrintUsage.txt");
+    try {
+      List<String> lines = Files.readAllLines(usagePath);
+      for (int i = 0; i < lines.size(); i++) {
+        System.out.println(lines.get(i));
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+    public void printToDo() {
+    Path listPath = Paths.get("list.txt");
+    try {
+      List<String> lines = Files.readAllLines(listPath);
+      if (lines.size() == 0) {
+        System.out.println("No todos for today");
+      }
+      for (int i = 0; i < lines.size(); i++) {
+        System.out.println(i+1 + " - " + lines.get(i));
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
