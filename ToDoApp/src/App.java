d3e18b3edf993190214940641360e07c19a69048
Kristof Majoros <akmajoros1@gmail.com>
2017-04-06 12:17:14 +0200

    read PrintUsage in command line

--- ToDoApp/src/App.java
+++ ToDoApp/src/App.java
@@ -1,8 +1,22 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
 
 public class App {
 
     public static void main(String[] args) {
-      System.out.println(Arrays.toString(args));
+
+      if (args.length == 0) {
+        Path filePath = Paths.get("PrintUsage.txt");
+        try {
+          List<String> lines = Files.readAllLines(filePath);
+          System.out.println(lines);
+        } catch (Exception e) {
+          e.printStackTrace();
+          System.out.println("Uh-oh, could not read the file!");
+        }
+      }
     }
 }
